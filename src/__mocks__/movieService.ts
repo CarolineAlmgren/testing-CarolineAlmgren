ceb3df193a4f9c42ea7228085911c5b080e3820d
Carolinealmgren <carroalmgren89@gmail.com>
2024-04-23 22:09:08 +0200

    Created mocked API 🎉

--- src/__mocks__/movieService.ts
+++ src/__mocks__/movieService.ts
@@ -1,14 +1,30 @@
 
-import { getData } from "../ts/services/movieService";
 import { IMovie } from "../ts/models/Movie";
 
 
-export const getMovies = async (searchText: string):Promise<IMovie[]> => {
-    //return new Promise(async()=> {
-       const movieData= await getData(searchText) 
-       return movieData;
+export const getMovies = async (searchText: string): Promise<IMovie[]> => {
+    return new Promise((resolve,reject) => {
+        if(searchText===""){
+            reject("Sökfältet får inte vara tomt")
+        } else {
+      resolve([
+        {
+          Title: "MockadFilm 1",
+          imdbID: "4321",
+          Type: "Film",
+          Poster: "Img",
+          Year: "2022",
+        },
+        {
+          Title: "MockadFilm 2",
+          imdbID: "8765",
+          Type: "Film",
+          Poster: "Img",
+          Year: "2023",
+        },
+      ]);
+    }
+    });
+   
         
-    
-}
-
-
+    }
